54ac70713fcc120957fe937e6a0e8a88ba452d52
Jonas D. Pedersen <jdp@dbi-net.dk>
2022-05-16 20:19:10 +0200

    Specflow test added

---
+++ Shapes.Specflow/Features/Square.feature
@@ -0,0 +1,18 @@
+﻿Feature: Square
+
+A short summary of the feature
+
+@SquareArea
+Scenario: Get area af square
+	Given the height is 20
+	And the width is 20
+	When the area is calculated
+	Then the result should be 400
+
+
+@SquareCircumference
+Scenario: Get circumference af square
+	Given the height is 20
+	And the width is 20
+	When the circumference is calculated
+	Then the result should be 80
---
+++ Shapes.Specflow/StepDefinitions/SquareStepDefinitions.cs
@@ -0,0 +1,46 @@
+using Shapes.Services;
+
+namespace Shapes.Specflow.StepDefinitions
+{
+    [Binding]
+    public sealed class SquareStepDefinitions
+    {
+        private readonly Square square;
+        private double _result;
+
+        public SquareStepDefinitions()
+        {
+            square = new Square();
+        }
+
+        [Given("the height is (.*)")]
+        public void GivenTheHeightIs(int number)
+        {
+            square.Height = number;
+        }
+
+        [Given("the width is (.*)")]
+        public void GivenWidthIs(int number)
+        {
+            square.Width = number;
+        }
+
+        [When("the area is calculated")]
+        public void WhenTheAreaIsCalculated()
+        {
+            _result = square.Area();
+        }
+
+        [When("the circumference is calculated")]
+        public void WhenTheCircumferenceIsCalculated()
+        {
+            _result = square.Circumference();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            _result.Should().Be(result);
+        }
+    }
+}
