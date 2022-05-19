e109baa1a564e7f8cdad8d92b7761ddad5434ef8
Jonas D. Pedersen <jdp@dbi-net.dk>
2022-05-19 14:00:27 +0200

    AreaService

---
+++ Shapes.Services/Services/AreaService.cs
@@ -0,0 +1,22 @@
+﻿using Shapes.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Services.Services
+{
+    public  class AreaService
+    {
+        public void CalculateArea(IEnumerable<IArea> shapes)
+        {
+            double result = 0.0;
+            foreach (IArea area in shapes)
+            {
+                result += area.Area();
+            }
+        }
+    }
+
+}
